ec9c5f17db70533b95bb38e2eb69cc3c88d76d29
Nikita Korba <no.korba@gmail.com>
2018-05-06 21:19:12 +0300

    Video #48

--- src/App.js
+++ src/App.js
@@ -12,31 +12,68 @@ class App extends Component {
     ]
   };
 
-  switchNameHandler() {
-    console.log('Was clicked!!!');
-  }
+  switchNameHandler = newName => {
+    //console.log("Was clicked!!!");
+    this.setState({
+      persons: [
+        { name: newName, age: 23 },
+        { name: "Alina", age: 23 },
+        { name: "Oleh", age: 23 },
+        { name: "Olena", age: 23 }
+      ]
+    });
+  };
+
+  nameChangedHandler = event => {
+    this.setState({
+      persons: [
+        { name: event.target.value, age: 23 },
+        { name: "Alina", age: 23 },
+        { name: "Oleh", age: 23 },
+        { name: "Olena", age: 23 }
+      ]
+    });
+  };
 
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm React app</h1>
         <p>This is really working</p>
-        <button onClick={this.switchNameHandler}>Switch name</button>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler("vasilian")}
+        >
+          Switch name
+        </button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this, "Hello")}
+          changed={this.nameChangedHandler}
         />
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Varan")}
         />
         <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age}
+          click={this.switchNameHandler.bind(this, "Petro")}
         />
         <Person
           name={this.state.persons[3].name}
           age={this.state.persons[3].age}
+          click={this.switchNameHandler.bind(this, "Baran")}
         >
           My hobbies: gardening
         </Person>
